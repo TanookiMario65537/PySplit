16358d9c8a93eedf0caa25698da1a582b05b6499
Tanooki Mario <tanookimario65537@gmail.com>
2023-10-02 15:51:19 -0400

    Fix the variance script for the new save data.

--- util/varianceCalculator.py
+++ util/varianceCalculator.py
@@ -3,33 +3,40 @@ from util import timeHelpers as timeh
 from util import readConfig as rc
 global varianceList
 
-def getRows(completeCsv):
+
+def getRows(saveData):
     rows = []
-    for i in range(len(completeCsv)):
+    for i in range(len(saveData["splitNames"])):
         timeList = []
-        for j in range(len(completeCsv[i])):
-            time = timeh.stringToTime(completeCsv[i][j])
+        for j in range(len(saveData["runs"])):
+            time = timeh.stringToTime(saveData["runs"][j]["segments"][i])
             if not timeh.isBlank(time):
                 timeList.append(time)
         rows.append(timeList)
     return rows
 
+
 def sort(i):
     return varianceList[i]
 
-def createTable(names,variances,sort):
+
+def createTable(names, variances, sort):
     table = []
     for i in sort:
-        table.append([names[i],variances[i]])
+        table.append([names[i], variances[i]])
     return table
 
-def computeVariances(game,category,splits):
+
+def computeVariances(game, category, splits):
     global varianceList
     config = rc.getUserConfig()
-    splitNames = splits.getSplitNames(game,category)
-    completeCsv = fileio.csvReadStart(config["baseDir"],game,category,splitNames)[0]
-    completeCsv = [[completeCsv[i][j] for j in range(1,len(completeCsv[i]),2)] for i in range(len(completeCsv))][1:]
-    timeRows = getRows(completeCsv)
+    splitNames = splits.getSplitNames(game, category)
+    saveData = fileio.readSplitFile(
+        config["baseDir"],
+        game,
+        category,
+        splitNames)
+    timeRows = getRows(saveData)
     varianceList = []
     for row in timeRows:
         if not len(row):
@@ -38,9 +45,18 @@ def computeVariances(game,category,splits):
         varList = [(x-avg)**2 for x in row]
         variance = sum(varList)/len(varList)
         varianceList.append(variance/avg)
-    sortedRange = sorted(list(range(len(varianceList))),key=sort,reverse=True)
+    sortedRange = sorted(
+        list(range(len(varianceList))),
+        key=sort,
+        reverse=True)
 
-    return {\
-        "order": createTable(splitNames,varianceList,range(len(varianceList))),\
-        "sorted": createTable(splitNames,varianceList,sortedRange)\
+    return {
+        "order": createTable(
+            splitNames,
+            varianceList,
+            range(len(varianceList))),
+        "sorted": createTable(
+            splitNames,
+            varianceList,
+            sortedRange)
     }
