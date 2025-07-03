cb8968d4b07892ea4c32816a7dea984ff1e74833
Tanooki Mario <tanookimario65537@gmail.com>
2025-07-02 22:33:24 -0400

    If using global hotkeys, create the socket and log all the inputs.

--- runTimer.py
+++ runTimer.py
@@ -6,29 +6,63 @@ from DataClasses import Session
 from States import State
 from util import readConfig as rc
 import logging
+import threading
+import socket
 
 
-def setHotkeys(app, state):
-    app.root.bind(
-        state.config["hotkeys"]["decreaseComparison"],
-        app.guiSwitchCompareCCW
-    )
-    app.root.bind(
-        state.config["hotkeys"]["increaseComparison"],
-        app.guiSwitchCompareCW
-    )
-    app.root.bind(state.config["hotkeys"]["split"], app.onSplitEnd)
-    app.root.bind(state.config["hotkeys"]["reset"], app.reset)
-    app.root.bind(state.config["hotkeys"]["skip"], app.skip)
-    app.root.bind(state.config["hotkeys"]["start"], app.start)
-    app.root.bind(state.config["hotkeys"]["pause"], app.togglePause)
-    app.root.bind(state.config["hotkeys"]["restart"], app.restart)
-    app.root.bind(state.config["hotkeys"]["finish"], app.finish)
-    app.root.bind(state.config["hotkeys"]["save"], app.save),
-    app.root.bind(state.config["hotkeys"]["partialSave"], app.partialSave),
-    app.root.bind("<Control-L>", app.partialLoad),
-    app.root.bind(state.config["hotkeys"]["chooseLayout"], app.chooseLayout)
-    app.root.bind(state.config["hotkeys"]["chooseRun"], app.chooseRun)
+class HotkeyHandler:
+    def __init__(self, app, state):
+        self.app = app
+        self.state = state
+        if self.state.config.get("globalHotkeys", False):
+            self.create_global_hotkeys()
+        else:
+            self.create_local_hotkeys()
+
+    def create_global_hotkeys(self):
+        logging.info("Starting global hotkey handler")
+        (
+            threading.Thread(
+                target=self.handle_global_hotkeys,
+                daemon=True
+            )
+            .start()
+        )
+
+    def create_local_hotkeys(self):
+        app.root.bind(
+            state.config["hotkeys"]["decreaseComparison"],
+            app.guiSwitchCompareCCW
+        )
+        app.root.bind(
+            state.config["hotkeys"]["increaseComparison"],
+            app.guiSwitchCompareCW
+        )
+        app.root.bind(state.config["hotkeys"]["split"], app.onSplitEnd)
+        app.root.bind(state.config["hotkeys"]["reset"], app.reset)
+        app.root.bind(state.config["hotkeys"]["skip"], app.skip)
+        app.root.bind(state.config["hotkeys"]["start"], app.start)
+        app.root.bind(state.config["hotkeys"]["pause"], app.togglePause)
+        app.root.bind(state.config["hotkeys"]["restart"], app.restart)
+        app.root.bind(state.config["hotkeys"]["finish"], app.finish)
+        app.root.bind(state.config["hotkeys"]["save"], app.save),
+        app.root.bind(state.config["hotkeys"]["partialSave"], app.partialSave),
+        app.root.bind("<Control-L>", app.partialLoad),
+        app.root.bind(
+            state.config["hotkeys"]["chooseLayout"],
+            app.chooseLayout
+        )
+        app.root.bind(state.config["hotkeys"]["chooseRun"], app.chooseRun)
+
+    def handle_global_hotkeys(self):
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.bind("/tmp/keyboard.sock")
+            logging.debug("Bound to local socket")
+            s.listen()
+            conn, _ = s.accept()
+            with conn:
+                for line in conn.makefile():
+                    logging.info(line.strip())
 
 
 def setupLogging():
@@ -61,7 +95,7 @@ while not app or exitCode:
     app = App.App(state, session)
     app.setupGui(showMenu=session.layout["menu"])
 
-    setHotkeys(app, state)
+    hotkeyHandler = HotkeyHandler(app, state)
     rootWindow = app.root
 
     loader = WidgetLoader.WidgetLoader(app, state, rootWindow)
